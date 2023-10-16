f6bc04837dcef920e2ca693df3540060deb6d2a6
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-10-16 20:39:53 +0300

    Add simulatePlan implementation

--- src/main/scala/retcalc/RetCalc.scala
+++ src/main/scala/retcalc/RetCalc.scala
@@ -1,6 +1,31 @@
 package retcalc
 
 object RetCalc {
+  def simulatePlan(interestRate: Double,
+                   nbOfMonthsSavings: Int,
+                   nbOfMonthsInRetirement: Int,
+                   netIncome: Int,
+                   currentExpenses: Int,
+                   initialCapital: Double): (Double, Double) = {
+    val capitalAtRetirement = futureCapital(
+      interestRate = interestRate,
+      nbOfMonths = nbOfMonthsSavings,
+      netIncome = netIncome,
+      currentExpenses = currentExpenses,
+      initialCapital = initialCapital
+    )
+
+    val capitalAfterDeath = futureCapital(
+      interestRate = interestRate,
+      nbOfMonths = nbOfMonthsInRetirement,
+      netIncome = 0,
+      currentExpenses = currentExpenses,
+      initialCapital = capitalAtRetirement
+    )
+
+    (capitalAtRetirement, capitalAfterDeath)
+  }
+
   def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome: Int,
                     currentExpenses: Int, initialCapital: Double): Double = {
     val monthlySavings = netIncome - currentExpenses
