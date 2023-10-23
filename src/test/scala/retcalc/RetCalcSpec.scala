82a4cb3b676c4d0172dae8cae75f95dbd1a853e5
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2023-10-23 20:42:31 +0300

    Change futureCapital to accept Returns type

--- src/test/scala/retcalc/RetCalcSpec.scala
+++ src/test/scala/retcalc/RetCalcSpec.scala
@@ -8,11 +8,10 @@ class RetCalcSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
 
   "RetCalc.futureCapital" should {
     "calculate the amount of savings I will have in n month" in {
-      val actual = RetCalc.futureCapital(
-        interestRate = 0.04 / 12, nbOfMonths = 25 * 12,
-        netIncome = 3000, currentExpenses = 2000,
-        initialCapital = 10000
-      )
+      // Exel =- FV(0.04/12,25*12,1000,10000,0)
+      val actual = RetCalc.futureCapital(FixedReturns(0.04),
+        nbOfMonths = 25 * 12, netIncome = 3000,
+        currentExpenses = 2000, initialCapital = 10000).right.value
       val expected = 541267.1990
 
       actual should ===(expected)
