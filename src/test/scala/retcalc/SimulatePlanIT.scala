e0e78179215b22cb1785322014982921f91d129b
BVynn <qbodia@gmail.com>
2024-02-05 20:38:22 +0400

    Implement SimulatePlanApp

--- src/test/scala/retcalc/SimulatePlanIT.scala
+++ src/test/scala/retcalc/SimulatePlanIT.scala
@@ -1,26 +1,20 @@
 package retcalc
 
 import org.scalactic.TypeCheckedTripleEquals
-import org.scalatest.{Matchers, fixture}
+import org.scalatest.{Matchers, WordSpec}
 
-class SimulatePlanIT extends fixture.WordSpec with Matchers with TypeCheckedTripleEquals {
+class SimulatePlanAppIT extends WordSpec with Matchers with TypeCheckedTripleEquals {
   "SimulatePlanApp.strMain" should {
     "simulate a retirement plan using market returns" in {
-      val actualResult = SimulatePlanIT.strMain(
-        Array("1997.09,2017.09", // The period
-              "25",              // The number of years of savings
-              "40",              // The number of years in retirement
-              "3000",            // Income
-              "2000",            // Expenses
-              "10000")           // Initial capital
-      )
+      val actualResult = SimulatePlanApp.strMain(
+        Array("1952.09,2017.09", "25", "40", "3000", "2000", "10000"))
+      // The period,The number of years of savings,The number of years in retirement,Income,Expenses,Initial capital
 
       val expectedResult =
         s"""
-           |Capital after 25 years of savings:    499923
-           |Capital after 40 years in retirement: 586435
-           """.stripMargin
-
+           |Capital after 25 years of savings:    468925
+           |Capital after 40 years in retirement: 2958842
+        """.stripMargin
       actualResult should === (expectedResult)
     }
   }
